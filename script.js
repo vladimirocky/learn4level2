3bdfc5b5c1364926273e17369e28c2eefec7848a
vrocky <vrocky53xapps.ru>
2022-03-06 20:22:03 +0300

    ready to learn

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
+/*
+ ДОПИШИТЕ КОД ФУНКЦИЙ И ИСПРАВЬТЕ ФУНКЦИЮ plus
+ */
+
 function  plus(){
-
-}function minus(){
-
+    // Для корректной работы
+    // Исправьте строку, и приведите значения к числам
+    let result = value1.value +  value2.value;
+    document.getElementById("res1").innerHTML = result;
+}
+function minus(){
+    // ваш код тут
+    let result = ""
+    // ваш код тут
+    document.getElementById("res1").innerHTML = result;
 }
 function multiplication(){
-
+    // ваш код тут
+    let result = ""
+    // ваш код тут
+    document.getElementById("res1").innerHTML = result;
 }
 function division(){
-
+    // ваш код тут
+    let result = ""
+    // ваш код тут
+    document.getElementById("res1").innerHTML = result;
 }
+// используйте операторы if и else
 function my_compare(){
-
+    // ваш код тут
+    let result = ""
+    // ваш код тут
+    document.getElementById("res1").innerHTML = result;
 }
 function my_strong(){
+    /* Сначала поптыаемся получить число,
+     Явно приводим значение инпута к числу
+     если ввод не число - заберем строку*/
+    let x = Number(value1.value) || String(value1.value);
+    let y = Number(value2.value) || String(value2.value);
+    let result = "";
 
+    (x !== y) ?
+        result = " не выполняется строгое равенство " : result = "выполняется строгое равенство";
+
+    document.getElementById("res1").innerHTML = result;
+}
+function logic_operators(){
+    // ваш код тут
+    let result = ""
+    // ваш код тут
+    document.getElementById("res2").innerHTML = result;
 }
