96bab81da0a3e9dbb94c9b8fe9c7143f8bb0b214
Ken Karlson <javarecords@mail.ru>
2022-03-10 11:59:14 +0300

    Add new logic script

--- script.js
+++ script.js
@@ -1,54 +1,88 @@
-/*
- ДОПИШИТЕ КОД ФУНКЦИЙ И ИСПРАВЬТЕ ФУНКЦИЮ plus
- */
+//Calc functions
 
-function  plus(){
+function plus() {
     // Для корректной работы
     // Исправьте строку, и приведите значения к числам
-    let result = value1.value +  value2.value;
+    let result = parseFloat(value1.value) + parseFloat(value2.value);
     document.getElementById("res1").innerHTML = result;
 }
-function minus(){
+
+function minus() {
     // ваш код тут
-    let result = ""
-    // ваш код тут
-    document.getElementById("res1").innerHTML = result;
-}
-function multiplication(){
-    // ваш код тут
-    let result = ""
+    let result = parseFloat(value1.value) - parseFloat(value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
-function division(){
+
+function multiplication() {
     // ваш код тут
-    let result = ""
-    // ваш код тут
-    document.getElementById("res1").innerHTML = result;
-}
-// используйте операторы if и else
-function my_compare(){
-    // ваш код тут
-    let result = ""
+    let result = parseFloat(value1.value) * parseFloat(value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
-function my_strong(){
+
+function division() {
+    // ваш код тут
+    let result = parseFloat(value1.value) / parseFloat(value2.value);
+    // ваш код тут
+    document.getElementById("res1").innerHTML = result;
+}
+
+// Сomparison functions
+
+function my_compare() {
+    // ваш код тут
+    // поля ввести "А" и "а"
+    let x = Number(value1.value) || String(value1.value);
+    let y = Number(value2.value) || String(value2.value);
+    //let x = value1.value;
+    //let y = value2.value;
+    let result = "";
+    (x != y) ?
+    result = "выполняется не строгое равенство ": result = "не выполняется равенство";
+    console.log(x);
+    console.log(y);
+    document.getElementById("res1").innerHTML = result;
+}
+
+function my_strong() {
     /* Сначала поптыаемся получить число,
      Явно приводим значение инпута к числу
      если ввод не число - заберем строку*/
+
+    // поля ввести "А" и "А"
     let x = Number(value1.value) || String(value1.value);
     let y = Number(value2.value) || String(value2.value);
     let result = "";
 
     (x !== y) ?
-        result = " не выполняется строгое равенство " : result = "выполняется строгое равенство";
+    result = " не выполняется строгое равенство ": result = "выполняется строгое равенство";
 
     document.getElementById("res1").innerHTML = result;
 }
-function logic_operators(){
-    // ваш код тут
-    let result = ""
+
+//Logic operators
+
+function logic_operators() {
+    let result = "";
+    let inputVal1 = val1.value;
+    let inputVal2 = val2.value;
+    let inputVal3 = val3.value;
+    let inputVal4 = val4.value;
+
+    if (inputVal1) {
+        result = inputVal1;
+    } else if (inputVal2) {
+        result = inputVal2;
+    } else if (inputVal3) {
+        result = inputVal3;
+    } else if (inputVal4) {
+        result = inputVal4;
+    } else {
+        result = "Undefine";
+    }
+
+
     // ваш код тут
     document.getElementById("res2").innerHTML = result;
 }
