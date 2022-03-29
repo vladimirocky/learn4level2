6d9fad7c69e1b2861d11402d3290ec2a76f50dac
Programist-is-Pochinka <investing_vk@vk.com>
2022-03-29 19:27:31 +0300

    learn4

--- script.js
+++ script.js
@@ -3,33 +3,40 @@
  */
 
 function  plus(){
-    // Для корректной работы
-    // Исправьте строку, и приведите значения к числам
-    let result = value1.value +  value2.value;
+    // Использую явное привидение типов, потому что при попытке сложить число и
+    //строку число конвертится в строку и 2 строки склеиваются
+    
+    let result = Number(value1.value) +  Number(value2.value);
     document.getElementById("res1").innerHTML = result;
 }
+
+//При вычитании, делении, умножении автоматическое привидение типов в int
 function minus(){
-    // ваш код тут
-    let result = ""
-    // ваш код тут
+    
+    let result = (value1.value) - (value2.value);
+   
     document.getElementById("res1").innerHTML = result;
 }
 function multiplication(){
-    // ваш код тут
-    let result = ""
-    // ваш код тут
+    
+    let result = (value1.value) * (value2.value);
+// ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 function division(){
-    // ваш код тут
-    let result = ""
-    // ваш код тут
+    
+    let result = (value1.value) / (value2.value);
+   
     document.getElementById("res1").innerHTML = result;
 }
-// используйте операторы if и else
+
 function my_compare(){
-    // ваш код тут
-    let result = ""
+    // Выполняется не строгое сравнение типов
+    if(value1.value == value2.value){
+        result = "Выполняется нестрогое равенство";
+    }else{
+        result = "Не выполняется нестрогое равенство";
+    }
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
@@ -39,16 +46,20 @@ function my_strong(){
      если ввод не число - заберем строку*/
     let x = Number(value1.value) || String(value1.value);
     let y = Number(value2.value) || String(value2.value);
-    let result = "";
-
-    (x !== y) ?
+    
+    //Выполняем не строгое сравнение потому что при сравнивание числа со строкой,
+    //обе переменные приводятся к одинаковому типу динамической типизацией
+    (x != y) ?
         result = " не выполняется строгое равенство " : result = "выполняется строгое равенство";
 
     document.getElementById("res1").innerHTML = result;
 }
 function logic_operators(){
     // ваш код тут
-    let result = ""
+    //переменной result присваивется только значение true 
+    //из пяти доступных вариантов 100% будет значение true даже если пользователь оставил строку пустой
+    let result = val1.value || val2.value || val3.value || val4.value || "не индефецировано";
     // ваш код тут
     document.getElementById("res2").innerHTML = result;
+    
 }
