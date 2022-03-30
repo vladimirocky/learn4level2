33179e13c8b2878c4736419124284be27fbe852f
AlexeyLyapin235 <95773691+AlexeyLyapin235@users.noreply.github.com>
2022-03-30 19:24:23 +0300

    Complete

--- script.js
+++ script.js
@@ -2,34 +2,42 @@
  ДОПИШИТЕ КОД ФУНКЦИЙ И ИСПРАВЬТЕ ФУНКЦИЮ plus
  */
 
-function  plus(){
+ function  plus(){
     // Для корректной работы
     // Исправьте строку, и приведите значения к числам
-    let result = value1.value +  value2.value;
+    let result = Number(value1.value) + Number (value2.value);
     document.getElementById("res1").innerHTML = result;
 }
 function minus(){
     // ваш код тут
-    let result = ""
+    let result = Number(value1.value) - Number(value2.value)
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 function multiplication(){
     // ваш код тут
-    let result = ""
+    let result = Number(value1.value) * Number (value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 function division(){
     // ваш код тут
-    let result = ""
+    let result = Number(value1.value) / Number (value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 // используйте операторы if и else
 function my_compare(){
     // ваш код тут
-    let result = ""
+    let x = Number(value1.value) && String(value1.value);
+    let y = Number(value2.value) && String(value2.value);
+    let result = "";
+   if(x != y){
+       result = "Выполняется не строгое равенство "
+       
+   } else{
+       result = "Не выполняется не строгое равенство "
+   };
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
@@ -48,7 +56,16 @@ function my_strong(){
 }
 function logic_operators(){
     // ваш код тут
-    let result = ""
+     if(val1.value != ''){
+         result = val1.value
+     } else if(val2.value != ''){
+         result=val2.value
+     } else if(val3.value != ''){
+         result = val3.value
+     } else if(val4.value != ''){
+         result = val4.value
+     }
+    
     // ваш код тут
     document.getElementById("res2").innerHTML = result;
 }
