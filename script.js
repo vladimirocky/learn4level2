568a1f4648618af5604454c5ce0dd611cb98a75c
Timur Markovnikov <gadzhibekovtimur@gmail.com>
2022-04-03 23:34:52 +0300

    did the task with the calculator

--- script.js
+++ script.js
@@ -5,32 +5,35 @@
 function  plus(){
     // Для корректной работы
     // Исправьте строку, и приведите значения к числам
-    let result = value1.value +  value2.value;
+    let result = Number(value1.value) + Number(value2.value);
     document.getElementById("res1").innerHTML = result;
 }
 function minus(){
     // ваш код тут
-    let result = ""
+    let result = Number(value1.value) - Number(value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 function multiplication(){
     // ваш код тут
-    let result = ""
+    let result = Number(value1.value) * Number(value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 function division(){
     // ваш код тут
-    let result = ""
+    let result = Number(value1.value) * Number(value2.value);
     // ваш код тут
     document.getElementById("res1").innerHTML = result;
 }
 // используйте операторы if и else
 function my_compare(){
     // ваш код тут
-    let result = ""
-    // ваш код тут
+    if (value1.value == value2.value){
+        result = "Выполняется нестрогое равенство";
+    } else{
+        result = "Не выполняется нестрогое равенство";
+    }
     document.getElementById("res1").innerHTML = result;
 }
 function my_strong(){
@@ -38,8 +41,7 @@ function my_strong(){
      Явно приводим значение инпута к числу
      если ввод не число - заберем строку*/
     let x = Number(value1.value) || String(value1.value);
-    let y = Number(value2.value) || String(value2.value);
-    let result = "";
+    let y = Number(value2.value) || String(value2.value);   
 
     (x !== y) ?
         result = " не выполняется строгое равенство " : result = "выполняется строгое равенство";
@@ -48,7 +50,7 @@ function my_strong(){
 }
 function logic_operators(){
     // ваш код тут
-    let result = ""
+    let result =  val1.value || val2.value || val3.value || val4.value || "undefined";
     // ваш код тут
     document.getElementById("res2").innerHTML = result;
 }
